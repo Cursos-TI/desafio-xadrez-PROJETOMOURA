78ce387b270d487285f74c36f29251ef496c39e7
Moura Gabriel <mouragabrielalves@gmail.com>
2025-10-05 19:57:32 +0000

    desafio_finalizado

---
+++ DESAFIO_XADREZ_MESTRE.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+//Foi criado um recurso de recursividade para cada peça: torre, bispo e rainha
+
+void moverTorre(int casaT)
+{
+    if (casaT > 0)
+    {
+        printf("Direita\n");
+        moverTorre(casaT - 1);
+    }
+    
+}
+
+void moverBispo(int casaB)
+{
+   if(casaB > 0)
+   {
+   printf("DIREITA\t");
+   printf("CIMA\n");
+   moverBispo(casaB - 1);
+   }
+}
+
+void moverRainha (int casaR)
+{
+    if (casaR > 0)
+    {
+        printf("ESQUERDA\n");
+        moverRainha(casaR - 1);
+    }
+}
+
+int main (){
+    
+    printf("\n===============<<<Nivel Mestre>>>==========\n");
+
+    printf("                    ==<<TORRE>>==\n");
+    
+
+    moverTorre(5);
+
+//Para o movimento do Bispo utilizando a estrutura de Recursividade
+
+   printf("\n===========================================\n");
+
+    printf("                    ==<<BISPO>>==\n");
+
+
+   moverBispo(5);
+
+   printf("\n-------------------------------------------\n");
+
+//Segue o exemplo do mesmo movimento para o Bispo, porém utilizando um Lop Aninhado em FOR
+
+int bispo;
+
+   for(int bispo = 0; bispo < 5; bispo++)
+   {
+    for (int movimentoBispo = 1; movimentoBispo <= 1; movimentoBispo++)
+    {
+        printf("DIREITA\t");
+    }
+    printf("CIMA\n");
+   }
+ printf("\n===========================================\n");
+ printf("                    ==<<RAINHA>>==\n");
+ 
+    
+ moverRainha(8);
+printf("\n===========================================\n");
+
+printf("                    ==<<CAVALO>>==\n");
+
+    int i, j;
+    
+    for(i = 0; i < 3; i++) // loop externo
+    {
+       for(j = 0; j < 3; j++) // loop interno
+        {
+        if(j >= 2) //se j for maior ou igual a 2 pula para a próxima iteração
+        continue; // pula o resto do loop interno
+        
+        printf("CIMA\n"); //imprime CIMA nas primeiras iterações válidas
+        }
+    printf("DIREITA\n");
+    break; //sai do loop externo
+    }
+       
+    
+
+
+return 0;
+}
--- DESAFIO_XADREZ_aventureiro.c
+++ DESAFIO_XADREZ_aventureiro.c
@@ -4,57 +4,6 @@ int main (){
     int bispo = 0, rainha = 0, cavalo;
     int movimento_cavalo, movimento_torre = 5, movimento_bispo = 5, movimento_rainha = 8;
     
-    printf("\n================================================\n");
-    printf("\n<<< Desafio: Movimentação de Peças do Xadrez >>>\n");
-    printf("            <<< Nível Iniciante >>>\n");
-
-    //Para movimentar a primeira peça, a TORRE, foi utilizado a função FOR
-    
-    printf("                =============\n");
-    printf("                ==<<TORRE>>==\n");
-    printf("                =============\n");
-    
-
-    //uma característica da função FOR é a possibilidade de criar e usar uma variável dentro do seu bloco
-     for (int torre = 0; torre < movimento_torre; torre++)
-     {
-        printf("Cima\n");
-       
-     }
-    printf("A TORRE se descolou %d casas para CIMA\n", movimento_torre);
- 
-     
-    //Foi utilizado a função while para movimentação do BISPO
-   
-    printf("                =============\n");
-    printf("                ==<<BISPO>>==\n");
-    printf("                =============\n");
-
-          
-    while (bispo < movimento_bispo)
-    {
-        printf("CIMA/DIREITA\n");
-    bispo++;
-    }
-
-    printf("O BISPO se deslocou %d casas na diagonal direita para cima\n", movimento_bispo);
-   
-    
-     //foi utilizado a função do-while para movimentação da Rainha
-    
-    printf("                ==============\n");
-    printf("                ==<<RAINHA>>==\n");
-    printf("                ==============\n");
-    
-    do
-    {
-        printf("ESQUERDA\n");
-        rainha++;
-
-    } while (rainha < movimento_rainha);
-    
-    printf("A RAINHA se deslocou %d casas para esquerda\n", movimento_rainha);
-    
 
     printf("\n===============<<<Nivel Aventureiro>>>==========\n");
     printf("                ==============\n");
@@ -85,13 +34,12 @@ while (cavalo < 2)
         printf("ESQUERDA\n"); 
         cavalo++;
     }
-    printf("\nCavalo se descola em L para baixo/esquerda\n");
-   
+      
    printf("\n================================================\n");
 
     //Loop Alinhado FOR 
     //foi criado as variáveis CAVALO e MOVIMENTO_CAVALO dentro da própria estrutura
-    //a fim de demonstração desta possibilidade ao utilizar a função FOR.
+    //a fim de demonstração desta possibilidade ao utilizar Loop Alinhado FOR.
     //Foi aplicada a mesma lógica do código anterior.
 
     for(int cavalo = 1; cavalo < 2; cavalo++)
@@ -101,7 +49,7 @@ while (cavalo < 2)
             printf("BAIXO\n");
         }
         printf("ESQUERDA\n");
-        printf("\nCavalo se descola em L para baixo/esquerda\n");
+        
     }
 
 
